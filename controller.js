2856d7a4679a3642a2821377b9f7cef7d8238d26
Sanket Choudhary <sanket.choudhary@walkinto.in>
2017-11-08 20:22:10 +0530

    with click event

--- controller.js
+++ controller.js
@@ -1,3 +1,8 @@
 var app = angular.module("infinitescroll", []);
 app.controller("appController", function($scope){
+    $scope.showMore = function(e){
+        console.log(e);
+        e.target.parentElement.style.height = "auto";
+        e.target.previousElementSibling.style.webkitLineClamp = "";
+    }
 });
